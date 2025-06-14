6241002fdcf2a5bb52f20b0003c7116b0858a564
Sarah Schulte <git@sarah.engineer>
2025-06-14 00:20:40 -0700

    add overlay thing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,20 @@ import { useWebsocket } from "./useWebsocket";
 import { DebugView } from "./DebugView";
 
 function App() {
-  useWebsocket("auto");
+  useWebsocket("production");
 
+  const hash = window.location.hash || "";
+  console.log(hash);
   return (
     <>
-      <DebugView />
+      {hash === "#overlay" ? (
+        <div className="overlay">
+          <iframe width="1920" height="1080" src="https://overlays.tiltify.com/9VqqHWOPJVIui04iA-a6tB1Ba7DDZVIO" />
+          <img src="overlay.png" />
+        </div>
+      ) : (
+        <DebugView />
+      )}
     </>
   );
 }
