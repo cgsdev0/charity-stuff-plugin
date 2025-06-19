ac664859227d1e4be5ca1b74cb4de974941e2d70
Colin Kautz <colin.kautz@gmail.com>
2025-06-18 22:51:13 -0500

    css cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,12 +13,12 @@ function App() {
   return (
     <>
       <PrimeReactProvider>
-        <div style={{ display: "flex", flexDirection: "row", position: "absolute"}}>
-          <div className="half" style={{ background: "linear-gradient(to top, #ed0852, #fe9902",  display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+        <div className="main">
+          <div className="half jake-half">
             <TeamHeader teamName="JAKE" alignment="flex-start" />
             <GoalsList teamName="JAKE" />
           </div>
-          <div className="half" style={{ background: "linear-gradient(to bottom, #02a8f8, #5b24e3)", display: "flex", flexDirection: "column", alignItems: "flex-end" }}>
+          <div className="half badcop-half">
             <TeamHeader teamName="BADCOP" alignment="flex-end" />
             <GoalsList teamName="BADCOP" />
           </div>
