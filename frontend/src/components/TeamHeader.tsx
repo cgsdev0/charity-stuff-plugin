a5cc41c507cb846f0882d4a323a0c40be4d2e61d
Sarah Schulte <git@sarah.engineer>
2025-06-25 19:36:00 -0700

    improved

--- frontend/src/components/TeamHeader.tsx
+++ frontend/src/components/TeamHeader.tsx
@@ -5,7 +5,7 @@ import { Tooltip } from "primereact/tooltip";
 
 export function TeamHeader({ teamName, alignment }: { teamName: string; alignment: "flex-start" | "flex-end" }) {
   const team = useStore(useShallow((state) => state.teams.find((team) => team.leader === teamName))) || { players: [] };
-  const playerNames = useStore(useShallow((state) => team.players.map((player) => state.players[player])));
+  const players = useStore((state) => state.players);
 
   if (teamName === "JAKE") teamName = "JAKECREATES";
 
@@ -14,12 +14,15 @@ export function TeamHeader({ teamName, alignment }: { teamName: string; alignmen
       <div className="team-header-container" style={{ alignItems: alignment }}>
         <h2 className="team-name">TEAM {teamName}</h2>
         <div className="player-icons" style={{ justifyContent: alignment }}>
-          {playerNames.map((player) => (
-            <span key={player}>
-              <Avatar image={`https://mc-heads.net/avatar/${player}`} size="normal" data-pr-tooltip={player} />
-              <Tooltip target={`[data-pr-tooltip=${player}]`} position={alignment == "flex-start" ? "right" : "left"} />
-            </span>
-          ))}
+          {team.players.map((uuid) => {
+            const name = players[uuid];
+            return (
+              <span key={uuid}>
+                <Avatar image={`https://mc-heads.net/avatar/${uuid}`} size="normal" data-pr-tooltip={name} />
+                <Tooltip target={`[data-pr-tooltip=${name}]`} position={alignment == "flex-start" ? "right" : "left"} />
+              </span>
+            );
+          })}
         </div>
       </div>
     </>
